b6ad7bcc2c3f272ed94f146b881cc95446ffa6d8
Matthew Berland <mberland@wisc.edu>
2024-03-06 13:07:13 -0600

    in class mods

--- poker/mycode.py
+++ poker/mycode.py
@@ -3,22 +3,22 @@ import tensorflow as tf
 # instantiate a model here
 
 model = 0
-def init_if_necessary()
-    if model == 0:
-        return tf.keras.models.load_model("mymodel.keras")
-    return model
+#def init_if_necessary()
+#    if model == 0:
+#        return tf.keras.models.load_model("mymodel.keras")
+#    return model
 
-def transmute(row):
-    return 7
+#def transmute(row):
+#    return 7
 
 def get_best_choice(row_of_data):
     # you write ALL YOUR CODE so it reports a number here
     # this can obviously call other functions
     # print(row_of_data)
-
-    model = init_if_necessary()
-    y_pred = model.predict(transmute(row_of_data))
-    choice = y_pred[0]
+#
+#    model = init_if_necessary()
+#    y_pred = model.predict(transmute(row_of_data))
+#    choice = y_pred[0]
     
     # transmute the row into something your tensorflow model can use
     # ask the model for the response in some way that generates a number
